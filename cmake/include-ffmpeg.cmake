9a9e2df6664f0fd6c2a51d519921a2de4a9753e8
bin <tuduweb@qq.com>
2021-12-23 13:39:47 +0800

    加入已编译的ffmpeg的cmake信息（初版）。

---
+++ cmake/include-ffmpeg.cmake
@@ -0,0 +1,28 @@
+# 已经编译好的方式
+
+set(ffmpeg_libs_DIR /usr/local/Cellar/ffmpeg/4.4.1_3/lib)
+set(ffmpeg_headers_DIR /usr/local/Cellar/ffmpeg/4.4.1_3/include)
+
+add_library(avutil SHARED IMPORTED)
+add_library(avcodec SHARED IMPORTED)
+add_library(avformat SHARED IMPORTED)
+add_library(swresample SHARED IMPORTED)
+add_library(swscale SHARED IMPORTED)
+add_library(avfilter SHARED IMPORTED)
+
+#指定所添加依赖库的导入路径
+set_target_properties( avcodec PROPERTIES IMPORTED_LOCATION ${ffmpeg_libs_DIR}/libavcodec.dylib )
+set_target_properties( avfilter PROPERTIES IMPORTED_LOCATION ${ffmpeg_libs_DIR}/libavfilter.dylib )
+set_target_properties( swresample PROPERTIES IMPORTED_LOCATION ${ffmpeg_libs_DIR}/libswresample.dylib )
+set_target_properties( swscale PROPERTIES IMPORTED_LOCATION ${ffmpeg_libs_DIR}/libswscale.dylib )
+set_target_properties( avformat PROPERTIES IMPORTED_LOCATION ${ffmpeg_libs_DIR}/libavformat.dylib )
+set_target_properties( avutil PROPERTIES IMPORTED_LOCATION ${ffmpeg_libs_DIR}/libavutil.dylib )
+
+# 添加头文件路径到编译器的头文件搜索路径下，多个路径以空格分隔
+include_directories( ${ffmpeg_headers_DIR} )
+link_directories(${ffmpeg_libs_DIR} )
+
+add_library(ffmpeg_lib STATIC)
+target_link_libraries(ffmpeg_lib
+    PUBLIC avcodec
+    avformat avutil swresample swscale swscale avfilter)
