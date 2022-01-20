dcd039521791b52bd0bbbc9f7538f0f2865fb51a
tududweb <tuduweb@qq.com>
2022-01-21 03:40:01 +0800

    更新调用ffmpeg。

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 
+extern "C" {
+	#include "libavcodec/avcodec.h"
+	#include "libavformat/avformat.h"
+}
+
 int main(int argc, char *argv[])
 {
     std::cout << "hello world" << std::endl;
+    std::cout << av_version_info() << std::endl;
+    std::cout << "hello world" << std::endl;
+
     return 0;
 }
